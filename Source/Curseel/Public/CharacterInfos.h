9d86bfd24aaacf285499c2dcd2d8372a46176f99
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2023-12-29 21:51:14 +0100

    intro

---
+++ Source/Curseel/Public/CharacterInfos.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterInfos.generated.h"
+
+UCLASS()
+class CURSEEL_API UCharacterInfos : public UDataAsset {
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditdefaultsOnly, BlueprintReadOnly)
+	FText CharacterName;
+
+	UPROPERTY(EditdefaultsOnly, BlueprintReadOnly)
+	UTexture2D* CharacterIcon;
+};
